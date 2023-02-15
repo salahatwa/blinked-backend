8996386156c020c290aabfbcd4c316b3c1a69841
SSATWA <SSATWA@DESKTOP-05GCN86>
2023-02-15 12:25:21 +0200

    run new profile

--- src/main/java/com/blinked/modules/core/config/WebConfiguration.java
+++ src/main/java/com/blinked/modules/core/config/WebConfiguration.java
@@ -1,11 +1,10 @@
 package com.blinked.modules.core.config;
 
-import java.util.Locale;
-
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.security.data.repository.query.SecurityEvaluationContextExtension;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -24,13 +23,15 @@ public class WebConfiguration implements WebMvcConfigurer {
 		return new SecurityEvaluationContextExtension();
 	}
 
+
+
 	@Bean
 	public MessageSource messageSource() {
-	    ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
-	    messageSource.setBasename("error-messages/messages");
-	    messageSource.setUseCodeAsDefaultMessage(true);
-	    messageSource.setDefaultEncoding("UTF-8");
-	    return messageSource;
+		ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+		messageSource.setBasename("error-messages/messages");
+		messageSource.setUseCodeAsDefaultMessage(true);
+		messageSource.setDefaultEncoding("UTF-8");
+		return messageSource;
 	}
 
 	@Value("${spring.thymeleaf.prefix}")
